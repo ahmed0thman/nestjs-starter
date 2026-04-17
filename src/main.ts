c2eef45e8a64af57afc1fd2f13624bc6753bc562
ahmed0thman <ahmedneo117@gmail.com>
2026-04-17 20:00:34 +0200

    using only I18nValidationPipe for validation

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { env } from './common/config/env/env';
-import { ValidationPipe } from '@nestjs/common';
 import { I18nValidationPipe } from 'nestjs-i18n';
 
 async function bootstrap() {
@@ -10,15 +9,7 @@ async function bootstrap() {
   // set global prefix for all routes
   const globalPrefix = `api/${env.API_VERSION}`;
   app.setGlobalPrefix(globalPrefix);
-  app.useGlobalPipes(
-    new I18nValidationPipe(),
-    new ValidationPipe({
-      whitelist: true, // strips unknown properties
-      forbidNonWhitelisted: true, // throws error for unknown properties
-      transform: true, // automatically converts types (like string -> number)
-    }),
-  );
-  // app.useGlobalFilters(new I18nValidationExceptionFilter());
+  app.useGlobalPipes(new I18nValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('My API')
